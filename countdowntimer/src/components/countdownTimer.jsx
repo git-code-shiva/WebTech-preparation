3099d2d0245b71f4f39dadb55140cdb93ac60a6b
shiva sharma <shva.sharma29@gmail.com>
2023-02-14 22:49:17 +0530

    added a feature so that when a user enter another input in middle it clears the prev interval

--- countdowntimer/src/components/countdownTimer.jsx
+++ countdowntimer/src/components/countdownTimer.jsx
@@ -1,65 +1,67 @@
 import React, { useEffect, useState } from "react";
-import './countDown.css'
+import "./countDown.css";
 
 const CountDownTimer = () => {
   const [timeCount, setTimeCount] = useState("");
   const [currentTime, setCurrentTime] = useState("");
+  const [intervalId, setIntervalId] = useState(null);
 
-  let interval;
+  // let interval;
 
-  const startTimer=()=>{
-    clearInterval(interval);
+  const startTimer = () => {
+    clearInterval(intervalId);
 
     let count = parseInt(timeCount);
 
-    if(isNaN(count)){
-        setCurrentTime("Invalid input");
-        setTimeCount("");
-        return;
-    }
-    else if(count <=0){
-        setCurrentTime("0");
-        setTimeCount("");
-        return;
+    if (isNaN(count)) {
+      setCurrentTime("Invalid input");
+      setTimeCount("");
+      return;
+    } else if (count <= 0) {
+      setCurrentTime("0");
+      setTimeCount("");
+      return;
     }
 
     setCurrentTime(count);
 
+    const id = setInterval(() => {
+      count = count - 1;
+      setCurrentTime(count);
+
+      if (count === 0) {
+        clearInterval(id);
+        setCurrentTime("Countdown Finished");
+      }
+    }, 1000);
     
-    interval=setInterval(()=>{
-        count = count-1;
-        setCurrentTime(count);
-
-        if(count ===0){
-            clearInterval(interval);
-            setCurrentTime("Countdown finshed");
-        }
-    },1000);
-
+    setIntervalId(id);
     setTimeCount("");
-  }
+  };
 
-//   useEffect(()=>{
-//     return ()=>{
-//         clearInterval(interval);
-//     };
-//   },[])
+  useEffect(() => {
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [intervalId]);
 
   return (
     <>
-    <h1>Countdown Timer</h1>
-    <div className="container">
-      <input
-        id="timeCount-inp"
-        type="text"
-        value={timeCount}
-        onChange={(e) => setTimeCount(e.target.value)}
-        onKeyDown={(e) => (e.key === "Enter" ? startTimer() : null)}
-      />
+      <h1>Countdown Timer</h1>
+      <div className="container">
+        <input
+          placeholder="Enter number here"
+          id="timeCount-inp"
+          type="text"
+          value={timeCount}
+          onChange={(e) => setTimeCount(e.target.value)}
+          onKeyDown={(e) => (e.key === "Enter" ? startTimer() : null)}
+        />
       </div>
 
-      <div id="show-timer">{currentTime===""?<p>Hit Enter to start the timer</p>:currentTime}</div>
-      
+      <div id="show-timer">
+        {currentTime === "" ? <p>Hit Enter to start the timer</p> : currentTime}
+      </div>
     </>
   );
 };
