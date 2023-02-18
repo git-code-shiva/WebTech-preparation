8b532304c2b6a7f4fc407c685b62b95376bd5285
shiva sharma <shva.sharma29@gmail.com>
2023-02-18 08:08:36 +0530

    bug fix

--- countdowntimer/src/App.js
+++ countdowntimer/src/App.js
@@ -1,12 +1,14 @@
 // import logo from './logo.svg';
 import React from 'react';
 import './App.css';
+// import AdvCountDownTimer from './components/advanceCounter';
 import CountDownTimer from './components/countdownTimer';
 
 function App() {
   return (
     <div className="App">
       <CountDownTimer/>
+      {/* <AdvCountDownTimer/> */}
     </div>
   );
 }
